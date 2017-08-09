03d14d07f476c86a73177aaea1193d0f169cabca
medvar <30646788+medvar@users.noreply.github.com>
2017-08-09 06:14:58 -0600

    mnaskdlkjlasdfsadfuoiuaeraadsf

--- form1/tipodevento.Designer.cs
+++ form1/tipodevento.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 297);
+            this.ClientSize = new System.Drawing.Size(467, 297);
             this.Name = "tipodevento";
             this.Text = "tipodevento";
             this.ResumeLayout(false);
