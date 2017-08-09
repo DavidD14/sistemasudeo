ddb6a1704a8c1cab7bdec074008955e33b3a432e
medvar <30646788+medvar@users.noreply.github.com>
2017-08-09 13:43:30 -0600

    as

--- form1/evento.Designer.cs
+++ form1/evento.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 295);
+            this.ClientSize = new System.Drawing.Size(664, 348);
             this.Name = "evento";
             this.Text = "evento";
             this.ResumeLayout(false);
