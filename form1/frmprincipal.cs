4597de751def3d207e797d678c3ba08d40a612f7
DavidD14 <juandavid@udeo.edu.gt>
2017-08-09 05:54:13 -0600

    v1

---
+++ form1/frmprincipal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace form1
+{
+    public partial class frmprincipal : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmprincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
